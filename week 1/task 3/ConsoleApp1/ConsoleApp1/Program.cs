7f3b1cf1c24a5a2a1d40d64b03adebbd9f8d0cd6
Sanzhar09 <S.adambekov09@gmail.com>
2019-02-10 00:37:56 +0600

    first commit

---
+++ week 1/task 3/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int n; // глобально обьявляем перменную для размера массива
+        static int[] a = new int[1000]; // создаем пустой масив с размером 1000
+        
+        static public void Nevoid() // создаем метод для того чтобы дублировать элементы массива 
+        {
+            int[] w = new int[2 * n]; // создаем еще один массив
+            for(int i=0;i<n;i++) // цикл чтобы присвоить и продублировать значения
+            {
+                w[i * 2] = a[i]; // текущий элемент пустого массива равен текущему элементу данного массива 
+                w[i * 2 + 1] = a[i]; // следуюищй элемент пустого массива равен текущему элементу данного массива
+            }
+            for(int i=0;i<w.Length; i++) // цикл чтобы вывксти элементы массива на экран
+            {
+                Console.Write(w[i] + " "); //выводим  
+            }
+        }
+        static void Main(string[] args) 
+        {
+            n = int.Parse(Console.ReadLine()); // считываем значение размера массива и переводим его в интежер
+            string[] p = Console.ReadLine().Split(); // создаем массив строк который разделяем по пробелу
+            for (int i = 0; i < n; i++) // цикл для присвоения значений массиву
+            {
+                a[i] = int.Parse(p[i]);// элемент массива равен элементу строки переведенный в интежер
+
+            }
+            Nevoid();// вызываем метод
+            Console.ReadKey();// чтобы консоль не закрылась
+        }
+    }
+}
---
+++ week 1/task 4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); // обьявляем размер массива и переводим его в интежер
+            string [,] a = new string [n,n]; // создаем двумерный массив размером n*n
+            for(int i=0;i<n;i++) // цикл для строк
+            {
+                for(int j=0;j<=i;j++) // цикл для столбцов
+                {
+                    a[i,j] = "[*]";
+                    Console.Write(a[i,j]);// чтобы вывести звездочки
+                    
+                }
+                Console.WriteLine(); // чтобы перейти на новую строку
+            }
+            Console.ReadKey(); // не закрылась чтобы.
+
+        }
+    }
+}
