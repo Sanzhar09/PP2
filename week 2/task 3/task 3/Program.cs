5a56ec12ec3a270952216e836f2b63f7a492c6a3
Sanzhar09 <S.adambekov09@gmail.com>
2019-02-16 05:26:37 +0600

    task 3

--- week 2/task 3/task 3/Program.cs
+++ week 2/task 3/task 3/Program.cs
@@ -3,13 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
-namespace task_3
+namespace Task3
 {
     class Program
     {
+        static void PrintSpaces(int lvl)
+        {
+            for (int i = 0; i < lvl; i++)
+                Console.Write(" ");
+        }
+        static void DirectoryTree(DirectoryInfo dir, int lvl)
+        {
+            FileInfo[] files = dir.GetFiles();
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            foreach (FileInfo file in files)
+            {
+                PrintSpaces(lvl);
+                Console.WriteLine(file.Name);
+            }
+            foreach (DirectoryInfo direct in dirs)
+            {
+                PrintSpaces(lvl);
+                Console.WriteLine(direct.Name);
+                DirectoryTree(direct, lvl++);
+            }
+        }
         static void Main(string[] args)
         {
+            DirectoryInfo path = new DirectoryInfo("D:/mashok/KBTU/PP2/");
+            DirectoryTree(path, 0);
+            Console.ReadKey();
         }
     }
 }
