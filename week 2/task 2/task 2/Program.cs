9e937e8a45e6da6aeb5a4781a81cee3975d927d8
Sanzhar09 <S.adambekov09@gmail.com>
2019-02-16 02:04:26 +0600

    task 2

--- week 2/task 2/task 2/Program.cs
+++ week 2/task 2/task 2/Program.cs
@@ -11,39 +11,40 @@ namespace task_2
     {
         static void Main(string[] args)
         {
-            StreamReader sr = new StreamReader("input.txt");
-            string[] s = sr.ReadToEnd().Split();
-            StreamWriter sw = new StreamWriter("output.txt");
-            List<int> ans = new List<int>();
-            for (int i = 0; i < s.Count(); i++)
+            StreamReader sr = new StreamReader("input.txt"); // используется для того чтобы считывать текст
+            string[] s = sr.ReadToEnd().Split(); // считываем текст
+            StreamWriter sw = new StreamWriter("output.txt"); // для того чтобы записывать текст
+            List<int> ans = new List<int>(); // создаем лист для праймов 
+            for (int i = 0; i < s.Count(); i++) // проходимся по всем числам(строкам) 
             {
-                int b;
-                int pr = 0;
-                b = int.Parse(s[i]);
-                for (int j = 2; j <= (int)Math.Sqrt(b); j++)
+                int b; // переменная для хранения числа
+                int pr = 0; // 0 если нет делители 
+                b = int.Parse(s[i]); // переводим из строки в число 
+                for (int j = 2; j <= (int)Math.Sqrt(b); j++) // цикл для проверки на прайм
                 {
-                    if (b % j == 0)
+                    if (b % j == 0) // если есть делитель 
                     {
-                        pr = 1;
-                        break;
+                        pr = 1; //то меняем значение переменной
+                        break; // выходим из цикла
                     }
 
                 }
-                if (pr == 0 && b != 1)
+                if (pr == 0 && b != 1) // если переменная 0(нет делителей) и наше число не равно 1
                 {
-                    ans.Add(b);
+                    ans.Add(b); // то записываем в лист
                 }
             }
 
 
 
 
-            for (int i = 0; i < ans.Count(); i++)
+            for (int i = 0; i < ans.Count(); i++) // проходимся по всем праймам в листе 
             {
-                sw.Write(ans[i] + " ");
+                sw.Write(ans[i] + " "); // записываем в файл
             }
-            sw.Close();
-            sr.Close();
+            sw.Close(); //закрываем streamwriter
+            sr.Close(); // закрываем stremreader
+            Console.ReadKey();
         }
     }
 }
